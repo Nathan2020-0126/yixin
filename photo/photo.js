4f8984d5069323a6f3e0aa8ff184b1db76e5b64d
x980344 <113729396@qq.com>
2018-02-21 21:29:26 +0800

    Site updated: 2018-02-21 21:29:23

--- photo/photo.js
+++ photo/photo.js
@@ -20,11 +20,11 @@ function draw_album() {
             for (let i = 0; i < data.length; i++) {
                 if (data[i].id == albumId) {
 
-                    let imagesHtmlStr = img_render_function(data[i].photos);
+                    let imagesHtmlStr = img_render_function(data[i].photos, data[i].prefix, data[i].classname);
                     $imagesContainer.html(imagesHtmlStr);
                     $(".images-container .album-title h2").html(data[i].title);
                     goto_images();
-                    console.log($imagesContainer.find("a.lightbox-link").length);
+
                     $imagesContainer.find("a.lightbox-link").fancybox({
                         "hideOnOverlayClick":false
                     });
@@ -46,6 +46,15 @@ function goto_albums() {
     $(".album-container").show();
 }
 
+function changeDisplayModel(model) {
+    var $imageItem = $(".album-detail .image");
+    $imageItem.removeClass("col-1");
+    $imageItem.removeClass("col-2");
+    $imageItem.removeClass("col-3");
+    $imageItem.removeClass("col-4");
+    $imageItem.addClass(model);
+}
+
 function album_render_function(data) {
     let albumn_htmlstr = "";
     for (let i = 0; i < data.length; i++) {
@@ -55,7 +64,7 @@ function album_render_function(data) {
 }
 
 function album_item_render_function(album){
-    let img_href=album.cover;
+    let img_href = album.prefix + album.cover;
     let htmlstr ='<div class="album">';
     htmlstr +='<div class="thumbnail">';
     htmlstr += '<a  class="lightbox-link" rel="album" href="' + img_href + '" title="' + album.description+'">';
@@ -69,16 +78,16 @@ function album_item_render_function(album){
     return htmlstr;
 }
 
-function img_render_function(images) {
+function img_render_function(images, prefix,classname) {
     let albumn_htmlstr = "";
     for (let i = 0; i < images.length; i++) {
-        albumn_htmlstr += img_item_render_function(images[i]);
+        albumn_htmlstr += img_item_render_function(images[i], prefix, classname);
     }
     return albumn_htmlstr;
 }
-function img_item_render_function(image) {
-    let img_href = image.link;
-    let htmlstr = '<div class="image">';
+function img_item_render_function(image, prefix, classname) {
+    let img_href = prefix + image.link;
+    let htmlstr = '<div class="image ' + classname +'">';
     htmlstr += '<div class="thumbnail">';
     htmlstr += '<a class="lightbox-link" rel="images" href="' + img_href + '" title="' + image.text +'">';
     htmlstr += '<img src="' + img_href + '">';
